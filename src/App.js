da6d1ae5680b9a0277debccac1c643971fb0e765
sirjmkitavi <kitavimjoseph@gmail.com>
2018-10-02 22:18:48 +0300

    Set up tab navigation

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+// import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+import Home from './components/Home';
+import Schedule from './components/Schedule';
+import Speakers from './components/Speakers';
+import Sponsors from './components/Sponsors';
+
+const TabNavigator =  createMaterialBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon size={26} name={'home'} style={{ color: tintColor }} />
+      )
+    }
+  },
+  Schedule: {
+    screen: Schedule,
+    navigationOptions: {
+      tabBarLabel: 'Schedule',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon size={26} name={'calendar-clock'} style={{ color: tintColor }} />
+      )
+    }
+  },
+  Speakers: {
+    screen: Speakers,
+    navigationOptions: {
+      tabBarLabel: 'Speakers',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon size={26} name={'account-star'} style={{ color: tintColor }} />
+      )
+    }},
+  Sponsors: {
+    screen: Sponsors,
+    navigationOptions: {
+      tabBarLabel: 'Sponsors',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon size={26} name={'cash'} style={{ color: tintColor }} />
+      )
+    }},
+}, {
+  initialRouteName: 'Home',
+  activeColor: '#FFD64C',
+  inactiveColor: 'white',
+  barStyle: { backgroundColor: 'black' },
+  tabBarOptions: {
+    showIcon: true,
+    showLabel: false,
+    upperCaseLabel: false,
+    tabStyle: {
+      backgroundColor: 'black',
+    }
+  },
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: 'bottom'
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <TabNavigator />
+      </View>
+    )
+  }
+}
---
+++ src/components/Schedule.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Schedule extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Schedule!</Text>
+      </View>
+    );
+  }
+}
+
+export default Schedule;
