cab32def0e6e7f475f4e37a8d4475e2182e29d39
sirjmkitavi <kitavimjoseph@gmail.com>
2018-10-03 04:29:35 +0300

    sponsor confetti

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+} from 'react-native';
 import Header from './common/Header';
+import Confetti from 'react-native-confetti';
 
 class Sponsors extends Component {
+  componentDidMount() {
+    if(this._confettiView) {
+       this._confettiView.startConfetti();
+    }
+  }
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Header title='Sponsors' />
-        <Text>Sponsors!</Text>
+        <ImageBackground
+          style={styles.bgImage}
+          source={require('../../assets/img/bg.png')}
+        >
+          <View style={{ flex: 1 }}>
+            <Confetti
+              ref={(node) => this._confettiView = node}
+              duration={3000}
+              timeout={10}
+              confettiCount={300}
+              untilStopped={true}
+            />
+          </View>
+          <Text>Sponsors!</Text>
+        </ImageBackground>
       </View>
     );
   }
 }
 
 export default Sponsors;
+
+const styles = StyleSheet.create({
+  bgImage: {
+    width: '100%',
+    height: '100%',
+  },
+})
